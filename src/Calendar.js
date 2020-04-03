392b7928b9483bc7054bbf12426b45744470aa59
WhatIsLove <z@z.ru>
2020-04-04 02:04:20 +0400

    commit

---
+++ src/Calendar.js
@@ -0,0 +1,380 @@
+import React from 'react'
+import moment from 'moment'
+
+import Events from './Events'
+import Week from './Week'
+import DayNames from './DayNames'
+
+import './App.css'
+import Plus from './plus.svg'
+import arrowBack from './arrow_back.svg'
+
+export default class Calendar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedMonth: moment(),
+            selectedDay: moment().startOf('day'),
+            selectedMonthEvents: [],
+            showEvents: false,
+            createEvent: false
+        }
+
+        this.previous = this.previous.bind(this)
+        this.next = this.next.bind(this)
+        this.addEvent = this.addEvent.bind(this)
+        this.showEvents = this.showEvents.bind(this)
+        this.showCalendar = this.showCalendar.bind(this)
+        this.goToCurrentMonthView = this.goToCurrentMonthView.bind(this)
+        this.takeValues = this.takeValues.bind(this)
+    }
+    
+    componentDidMount() {
+        let local = JSON.parse(localStorage.getItem('CalendarEvents'))
+        if (local) {
+            const monthEvents = this.state.selectedMonthEvents
+
+            let allEvents = []
+
+            local.forEach(function(elem, i) {
+                let format = {
+                    date: moment(elem.date),
+                    title: elem.title,
+                    time: elem.time,
+                    people: elem.people,
+                    description: elem.description,
+                    dynamic: elem.dynamic
+                }
+                allEvents.push(format)
+
+                for (let i=0; i<allEvents.length; i++) {
+                    monthEvents.push(allEvents[i])
+                }
+            }) 
+            this.setState({ selectedMonthEvents : allEvents});
+        }
+    }
+    previous() {
+        const currentMonthView = this.state.selectedMonth
+        this.setState({ selectedMonth: currentMonthView.subtract(1, 'month')})
+    }
+    next() {
+        const currentMonthView = this.state.selectedMonth
+        this.setState({ selectedMonth: currentMonthView.add(1, 'month')})
+    }
+    select(day) {
+        this.setState({
+            selectedMonth: day.date,
+            selectedDay: day.date.clone(),
+            showEvents: true
+        })
+    }
+    goToCurrentMonthView() {
+        this.setState({selectedMonth: moment()})
+    }
+    showCalendar() {
+        this.setState({
+            selectedMonth: this.state.selectedMonth,
+            selectedDay: this.state.selectedDay,
+            showEvents: false,
+            createEvent: false,
+        })
+    }
+    showEvents() {
+        this.setState({
+            selectedMonth: this.state.selectedMonth,
+            selectedDay: this.state.selectedDay,
+            showEvents: true,
+            createEvent: false,
+        })
+    }
+    renderMonthLabel() {
+        const state = this.state.selectedMonth
+        let current = state.format('MMMM YYYY')
+        return (
+            <span onClick={e => {this.showMonth()}} className='box month-label'>
+                {current}
+            </span>
+        )
+    }
+    renderDayLabel() {
+        const state = this.state.selectedDay
+        let current = state.format('DD MMMM YYYY')
+        return (
+            <span className='box month-label'>
+                {current}
+            </span>
+        )
+    }
+    renderTodayLabel() {
+        return (
+            <span className='box today-label' onClick={this.goToCurrentMonthView}>
+                Сегодня
+            </span>
+        )
+    }
+    renderWeeks() {
+        const currentMonthView = this.state.selectedMonth
+        const currentSelectedDay = this.state.selectedDay
+        const monthEvents = this.state.selectedMonthEvents
+
+        let weeks = []
+        let done = false
+        let previousCurrentNextView = currentMonthView
+            .clone()
+            .startOf('month')
+            .subtract(1, 'd')
+            .day('Monday').locale('ru')
+        let count = 0
+        let monthIndex = previousCurrentNextView.month()
+
+        while (!done) {
+            weeks.push(
+                <Week 
+                    previousCurrentNextView={previousCurrentNextView.clone()}
+                    currentMonthView={currentMonthView}
+                    monthEvents={monthEvents}
+                    selected={currentSelectedDay}
+                    select={day => this.select(day)}
+                    key={count}
+                />
+            )
+            previousCurrentNextView.add(1, 'w')
+            done = count++ > 2 && monthIndex !==previousCurrentNextView.month()
+            monthIndex = previousCurrentNextView.month()
+        }
+        return weeks
+    }
+    addEvent() {
+        const currentSelectedDate = this.state.selectedDay
+        const createEvent = this.state.createEvent
+        let isAfterDay = moment().startOf('day').subtract(1, 'd')
+
+        if (currentSelectedDate.isAfter(isAfterDay)) {
+           this.setState({ createEvent: !createEvent })
+        } else {
+            if (window.confirm('Вы хотите создать событие в прошлом?')) {
+                this.setState({ createEvent: !createEvent })
+            }
+        }
+    }
+    removeEvent(i) {
+        const monthEvents = this.state.selectedMonthEvents.slice()
+
+        if (window.confirm('Вы хотите удалить это событие?')) {
+            let index = i
+
+            if (index !== -1) {
+                monthEvents.splice(index, 1)
+            } else {
+                alert('Нет событий для удаления на этот день')
+            }
+
+            this.setState({ selectedMonthEvents: monthEvents })
+            localStorage.setItem('CalendarEvents', JSON.stringify(monthEvents));
+        }
+    }
+    editEvent(i) {
+        const monthEvents = this.state.selectedMonthEvents
+        const idx = i
+
+        for (let i=0; i<monthEvents.length; i++) {
+            monthEvents[idx].dynamic = true
+        }
+        this.setState({monthEvents : monthEvents}) 
+    }
+    takeValues() {
+        let name = this.inputName.value;
+        let hours = this.inputHours.value;
+        let minutes = this.inputMinutes.value;
+        let people = this.inputPeople.value;
+        let description = this.inputDescription.value;
+
+        const monthEvents = this.state.selectedMonthEvents
+        const currentSelectedDate = this.state.selectedDay
+    
+        const newEvents = []
+
+        if (
+            (name.length === 0) ||
+            (isNaN(parseInt(hours)) || parseInt(hours) > 23 || hours.length === 0) ||
+            (isNaN(parseInt(minutes)) || parseInt(minutes) > 59 || minutes.length === 0)) {
+
+            if (name.length === 0) {
+                document.getElementById('eventName').style.border = '1px solid red'
+                setTimeout(() => {
+                    document.getElementById('eventName').style.border = '1px solid white'
+                }, 1500)
+            }
+    
+            if (isNaN(parseInt(hours)) || parseInt(hours) > 23 || hours.length === 0) {
+                document.getElementById('timeHours').style.border = '1px solid red'
+                setTimeout(() => {
+                    document.getElementById('timeHours').style.border = '1px solid white'
+                }, 1500)
+            }
+    
+            if (isNaN(parseInt(minutes)) || parseInt(minutes) > 59 || minutes.length === 0) {
+                document.getElementById('timeMinutes').style.border = '1px solid red'
+                setTimeout(() => {
+                    document.getElementById('timeMinutes').style.border = '1px solid white'
+                }, 1500)
+            }
+        } 
+        else {
+            let newEvent = {
+                title:  name,
+                time: `${hours}:${minutes}`,
+                people: people,
+                description: description,
+                date: currentSelectedDate,
+                dynamic: false
+            }
+            newEvents.push(newEvent)
+
+            for (let i=0; i< newEvents.length; i++) {
+                monthEvents.push(newEvents[i])
+            }
+            this.setState({ 
+                selectedMonthEvents: monthEvents,
+                createEvent: false
+            })
+            localStorage.setItem('CalendarEvents', JSON.stringify(monthEvents));
+        }
+    }
+    updateData = (events) => {
+        this.setState({ selectedMonthEvents: events })
+        localStorage.setItem('CalendarEvents', JSON.stringify(events));
+     }
+
+
+    render() {
+        const showEvents = this.state.showEvents
+        const createEvent = this.state.createEvent
+
+         if (createEvent) {
+                return (
+                    <div className='calendar_container'>
+                    <section className='main-calendar'>
+                        <header className='calendar-header'>
+                            <div className='row title-header'>
+                                {this.renderDayLabel()}
+                            </div>
+                            <div className='row button-container'>
+                                <div className='svg_wrap plus_svg' onClick={this.showEvents}><img className='plus_svg arrow_back' src={arrowBack} alt="arrowBack" /></div>
+                            </div>
+                        </header>
+
+                        <div className='create_event_container'>
+
+                            <div className='event_name'>
+                                <label>Название события:</label>
+                                <input 
+                                style={{width: '35%'}} 
+                                id='eventName' type='text' 
+                                className='event_input animError' 
+                                placeholder='Сходить в магазин' 
+                                ref={ref => this.inputName = ref}
+                                />
+                            </div>
+        
+                            <div className='event_time'>
+                                <label>Время события:</label>
+                                <input 
+                                style={{width: '5%'}} 
+                                type='text'
+                                className='event_input animError' 
+                                id='timeHours' 
+                                maxLength='2' 
+                                placeholder='12'
+                                ref={ref => this.inputHours = ref}
+                                />
+                                <span>:</span>
+                                <input 
+                                style={{width: '5%'}} 
+                                type='text' 
+                                className='event_input animError'
+                                maxLength='2' 
+                                placeholder='00'
+                                id='timeMinutes' 
+                                ref={ref => this.inputMinutes = ref}
+                                />
+                            </div>
+        
+                            <div className='event_people'>
+                                <label>Участники:</label>
+                                <input 
+                                type='text' 
+                                id='people' 
+                                className='event_input' 
+                                placeholder='Иван, Пётр'
+                                ref={ref => this.inputPeople = ref}
+                                />
+                            </div>
+        
+                            <div className='event_description'>
+                                <label>Описание:</label>
+                                <input 
+                                type='text' 
+                                id='description' 
+                                className='event_input' 
+                                placeholder='Купить хлеба и молока'
+                                ref={ref => this.inputDescription = ref}
+                                />
+                            </div>
+        
+                            <button onClick={this.takeValues} className='event_button'>Создать событие</button>
+                        </div>
+                    </section>
+                    </div>
+                )
+        }
+        if (showEvents) {
+            return (
+                <div className='calendar_container'>
+                    <section className='main-calendar'>
+                        <header className='calendar-header'>
+                            <div className='row title-header'>
+                                {this.renderDayLabel()}
+                            </div>
+                            <div className='button-container'>
+                                <div className='svg_wrap plus_svg' onClick={this.showCalendar}><img className='plus_svg arrow_back' src={arrowBack} alt="arrowBack" /></div>
+                                <div className='svg_wrap plus plus_svg' onClick={this.addEvent}><img className='plus_svg' src={Plus} alt="plus" /></div>
+                            </div>
+                        </header>
+                        <Events 
+                            selectedMonth={this.state.selectedMonth}
+                            selectedDay={this.state.selectedDay}
+                            selectedMonthEvents={this.state.selectedMonthEvents}
+                            removeEvent={i => this.removeEvent(i)}
+                            editEvent={i => this.editEvent(i)}
+                            updateData={this.updateData}
+                        />
+                    </section>
+                </div>
+            )
+        } else {
+            return (
+                <div className='calendar_container'>
+                    <section className='main-calendar'>
+                        <header className='calendar-header'>
+                            <div className='row title-header'>
+                                <span className='box arrow-left' onClick={this.previous}>{'<'}</span>
+                                <div className='box header-text'>
+                                    {this.renderTodayLabel()}
+                                    {this.renderMonthLabel()}
+                                </div>
+                                <span className='box arrow-right' onClick={this.next}>{'>'}</span>
+                            </div>
+                            <DayNames />
+                        </header>
+                        <div className='days-container'>
+                            {this.renderWeeks()}
+                        </div>
+                    </section>
+                </div>
+            )
+        }
+    }
+}
---
+++ src/Events.js
@@ -0,0 +1,197 @@
+import React from 'react'
+import Trash from './trash.svg'
+import Edit from './edit.svg'
+
+export default class Events extends React.Component {
+        state = {
+
+        }
+
+        editValues = () => {
+            let name = this.inputName.value;
+            let hours = this.inputHours.value;
+            let minutes = this.inputMinutes.value;
+            let people = this.inputPeople.value;
+            let description = this.inputDescription.value;
+    
+            const monthEvents = this.props.selectedMonthEvents 
+        
+            if (
+                (name.length === 0) ||
+                (isNaN(parseInt(hours)) || parseInt(hours) > 23 || hours.length === 0) ||
+                (isNaN(parseInt(minutes)) || parseInt(minutes) > 59 || minutes.length === 0)) {
+    
+                if (name.length === 0) {
+                    document.getElementById('eventName').style.border = '1px solid red'
+                    setTimeout(() => {
+                        document.getElementById('eventName').style.border = '1px solid white'
+                        document.getElementById('eventName').style.borderBottom = '1px solid #4B0082'
+                    }, 1500)
+                }
+        
+                if (isNaN(parseInt(hours)) || parseInt(hours) > 23 || hours.length === 0) {
+                    document.getElementById('timeHours').style.border = '1px solid red'
+                    setTimeout(() => {
+                        document.getElementById('timeHours').style.border = '1px solid white'
+                        document.getElementById('timeHours').style.borderBottom = '1px solid #4B0082'
+                    }, 1500)
+                }
+        
+                if (isNaN(parseInt(minutes)) || parseInt(minutes) > 59 || minutes.length === 0) {
+                    document.getElementById('timeMinutes').style.border = '1px solid red'
+                    setTimeout(() => {
+                        document.getElementById('timeMinutes').style.border = '1px solid white'
+                        document.getElementById('timeMinutes').style.borderBottom = '1px solid #4B0082'
+                    }, 1500)
+                }
+            } 
+            else {
+                monthEvents.map((event, i) => {
+                    if (event.dynamic) {
+        
+                        let eventEdit = {
+                            title: name,
+                            time: `${hours}:${minutes}`,
+                            people: people,
+                            description: description,
+                            date: event.date,
+                            dynamic: false
+                        }
+                        const monthEventsEdit = monthEvents.slice()
+                        let index = i
+                        
+                        event[i] = eventEdit
+                        monthEventsEdit.splice(index, 1, event[i])
+                        
+                        this.setState({ 
+                            monthEvents: monthEventsEdit
+                        })   
+                    }
+                    
+                })                 
+        }
+        document.querySelector('.event_button').style.display = 'none'
+        document.querySelector('.hide').style.display = 'flex'
+    }
+
+    render() {
+        const currentSelectedDay = this.props.selectedDay
+        const monthEvents = this.props.selectedMonthEvents
+        const removeEvent = this.props.removeEvent
+        const editEvent = this.props.editEvent
+        
+        const monthEventsRendered = monthEvents.map((event, i) => {
+            if (event.dynamic) {
+                return (
+                    <div key={i} className='event-container'>
+                        <input 
+                        style={{width: '28%', border: '0', borderBottom: '1px solid #4B0082', textDecoration: 'none', margin: '0 auto', textAlign: 'center', color: 'black'}} 
+                        id='eventName' type='text' 
+                        className='event_title_label animError' 
+                        defaultValue={event.title}
+                        ref={ref => this.inputName = ref}
+                        />
+
+                        <div className='event_info_wrapper'>
+                            <div>
+                                <div className='event_time_label' style={{display: 'block'}}>
+                                    <span style={{color: 'black', display: 'block'}}>Время: </span>
+
+                                    <input 
+                                    style={{width: '7%', color: 'black', border: '0', borderBottom: '1px solid #4B0082'}} 
+                                    type='text'
+                                    className='event_time_label animError' 
+                                    id='timeHours' 
+                                    maxLength='2' 
+                                    defaultValue={'12'}
+                                    ref={ref => this.inputHours = ref}
+                                    />
+                                    <span>:</span>
+                                    <input 
+                                    style={{width: '7%', border: '0', borderBottom: '1px solid #4B0082', color: 'black'}} 
+                                    type='text' 
+                                    className='event_time_label animError'
+                                    maxLength='2' 
+                                    defaultValue={'00'}
+                                    id='timeMinutes' 
+                                    ref={ref => this.inputMinutes = ref}
+                                    />
+
+                                    </div>
+                            </div>
+                            <div>
+                                <div className='event_people_label'>
+                                    <span style={{color: 'black'}}>Участники: </span>
+
+                                    <input 
+                                    type='text' 
+                                    id='people'
+                                    style={{width: '65%', border: '0', borderBottom: '1px solid #4B0082', textAlign: 'center', color: 'black'}}
+                                    className='event_people_label' 
+                                    defaultValue={event.people}
+                                    ref={ref => this.inputPeople = ref}
+                                    />
+
+                                </div>
+                            </div> 
+                        </div>
+                        <div className='event_description_label' style={{marginTop: '10px', textAlign: 'center'}}>
+                            <input 
+                            type='text' 
+                            id='description' 
+                            className='event_description_label' 
+                            style={{border: '0', borderBottom: '1px solid #4B0082'}}
+                            defaultValue={event.description}
+                            ref={ref => this.inputDescription = ref}
+                            />             
+                        </div>
+                        <button onClick={this.editValues} className='event_button'>Сохранить</button> 
+                        <button onClick={() => { this.props.updateData(this.state.monthEvents)}} className='event_button hide'>Закрыть</button>
+                    </div>
+                )
+            } else {
+                return (
+                    <div key={i} className='event-container'>
+
+                        <div className='event_title_label'>
+                            <img onClick={() => editEvent(i)} className='edit_svg' src={Edit} alt="edit" />
+                            {event.title}
+                            <img onClick={() => removeEvent(i)} className='trash_svg' src={Trash} alt="trash" />
+                        </div>
+                        
+                        <div className='event_info_wrapper'>
+                            <div>
+                                <div className='event_time_label'><span style={{color: 'black'}}>Время: </span>{event.time}</div>
+                            </div>
+                            <div>
+                                <div className='event_people_label'><span style={{color: 'black'}}>Участники: </span>{event.people}</div>
+                            </div> 
+                        </div>
+                        <div className='event_description_label'>{event.description}</div>
+                    </div>
+                )
+            }
+        })
+        const dayEventsRendered = []
+
+        for (let i=0; i<monthEventsRendered.length; i++) {
+            if (monthEvents[i].date.isSame(currentSelectedDay, 'day')) {
+                dayEventsRendered.push(monthEventsRendered[i])
+            }
+        }
+
+        if (dayEventsRendered.length === 0) {
+            return (
+                <div className='events-empty'>
+                    {dayEventsRendered}
+                </div>
+            )
+        } else {
+            return (
+                <div className='day-events'>
+                    {dayEventsRendered}
+                </div>
+            )
+        }
+    }
+}
